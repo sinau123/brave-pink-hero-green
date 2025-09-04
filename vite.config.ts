bd5e55ac4d6d717cf343ca638059b8240e8e287f
Denis Eko Harbiyanto <sinau123@gmail.com>
2025-09-04 17:49:58 +0700

    enable compression

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,24 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(), 
+    // Gzip
+    viteCompression({
+      algorithm: "gzip",
+      ext: ".gz",
+      threshold: 10240,
+    }),
+    // Brotli
+    viteCompression({
+      algorithm: "brotliCompress",
+      ext: ".br",
+      threshold: 10240,
+    }),
+  ],
 });
